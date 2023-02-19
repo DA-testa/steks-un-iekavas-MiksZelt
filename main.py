e81aaf6f636b7387468e2f30d1e4c86000c0dc62
Miks Zeltins <zeltins.miks24@gmail.com>
2023-02-19 23:32:58 +0200

    change6

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ def find_mismatch(text):
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i + 1
+            
             opening_brackets_stack.pop()
         
-        if not opening_brackets_stack:
+        if(not opening_brackets_stack):
             return "Success"
         else:
             return opening_brackets_stack[-1].position
