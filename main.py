66993cfe6bdc50eea618794d9c558f5e56bcc66f
Miks Zeltins <zeltins.miks24@gmail.com>
2023-02-19 23:23:11 +0200

    pabeigts

--- main.py
+++ main.py
@@ -16,19 +16,18 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next , i + 1))
-            
-        if next in ")]}":
+            pass
 
+        if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i + 1
             opening_brackets_stack.pop()
         
-    if(not opening_brackets_stack):
-        return "Success"
-    
-    else:
-        return opening_brackets_stack[-1].position
-        
+        if(opening_brackets_stack):
+            return opening_brackets_stack[0].position
+        else:
+            return "Success"
+    pass
 
 
 def main():
