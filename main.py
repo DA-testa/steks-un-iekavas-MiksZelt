14a92d57fdb5b9a2abc5f8924e06e993c79fa388
Miks Zeltins <zeltins.miks24@gmail.com>
2023-02-19 23:12:14 +0200

    change4

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(next)
+            opening_brackets_stack.append(Bracket(next , i + 1))
             
         if next in ")]}":
 
