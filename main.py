e10048050ea43c969fb5cd4ef9ee7750f7968c98
Miks Zeltins <zeltins.miks24@gmail.com>
2023-02-19 23:10:16 +0200

    change4

--- main.py
+++ main.py
@@ -15,14 +15,17 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next , i + 1))
+            opening_brackets_stack.append(next)
             
         if next in ")]}":
+
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i + 1
             opening_brackets_stack.pop()
+        
     if(not opening_brackets_stack):
         return "Success"
+    
     else:
         return opening_brackets_stack[-1].position
         
@@ -31,20 +34,20 @@ def find_mismatch(text):
 def main():
     text = input()
 
-    if "F" in text:
-        file = input()
-        with open(file) as f:       
-            line = file.readLine()
-        mismatch = find_mismatch(line)
+    if "I" in text:
+        text = input()
+        mismatch = find_mismatch(text)
 
         if not mismatch:
             print("Success")
         else:
             print(mismatch)
 
-    elif "I" in text:
-        text = input()
-        mismatch = find_mismatch(text)
+    elif "F" in text:
+        file = input()
+        with open(file) as f:       
+            line = file.readLine()
+        mismatch = find_mismatch(line)
 
         if not mismatch:
             print("Success")
